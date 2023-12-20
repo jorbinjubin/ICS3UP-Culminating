4a0c4310d4f27f8a9fbb2a7b2a4fe2f8d09018cb
jorbinjubin <142069933+jorbinjubin@users.noreply.github.com>
2023-12-20 14:30:00 -0500

    fei you suck

--- main.java
+++ main.java
@@ -1,17 +1,23 @@
-public static void main (String[] args){
-    RoadSigns r = new RoadSigns();
-    r.splashScreen();
-    while (true) {
-	r.mainMenu();
-	if (r.menuChoice == 1) break;
-	else if (r.menuChoice == 2) r.instruction ();
-    //     else if (r.menuChoice == 3) r.viewLeaderboard ();
-    //     else {
-    //         if(!r.skipLearning) r.learning();
-    //         if(!r.skipGameLoop) r.maze();
-    //         r.gameLoop();
-    //     }
-    // }
-    // r.goodbye ();
-}
+import java.io.*;
+import hsa.Console;
+import hsa.Message;
+import java.util.*;
 
+public class Main {
+	public static void main (String[] args){
+    	RoadSigns r = new RoadSigns();
+    	r.splashScreen();
+    	while (true) {
+		r.mainMenu();
+		if (r.menuChoice == 1) break;
+		else if (r.menuChoice == 2) r.instruction ();
+    	//     else if (r.menuChoice == 3) r.viewLeaderboard ();
+    	//     else {
+    	//         if(!r.skipLearning) r.learning();
+    	//         if(!r.skipGameLoop) r.maze();
+    	//         r.gameLoop();
+    	//     }
+    	// }
+    	// r.goodbye ();
+	}	
+}
