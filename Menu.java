2ce253d171c7a3d61d44e29368d4920707bc7511
jorbinjubin <142069933+jorbinjubin@users.noreply.github.com>
2023-12-22 15:00:20 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,57 +1,80 @@
 import hsa.Console; // or import hsa.*;
+import hsa.Message;
 import java.awt.*;
 import sun.audio.*;
 import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class Menu {
     AudioStream as;
-    int choice = 0;
+    static int choice = 0;
+    private Console c;
+    static boolean hasChosen = false;
+    public Menu(Console con) {
+        c = con;
+    }
+
     public void delay() {
-	try {
-	    Thread.sleep(220);
-	} catch(Exception e) {
-	}
+        try {
+            Thread.sleep(220);
+        } catch (Exception e) {}
+    }
+    private void drawOutline(int n) {
+        c.setColor(new Color(150, 150, 150));
+        for(int i = 0; i < 10; i++) {
+            c.drawRect(0+i, 149+152*n+i, 1024-2*i, 160-2*i);
+        }
     }
     public void menuSelect() {
-	boolean hasChosen = false;       
-	while (!hasChosen) {
-	    try {
-		if (c.isCharAvail()) {
-		    switch (c.getChar()) {
-		    case 'w':
-			if (choice < 4) {
-			    choice++;
-			    try {
-				as = new AudioStream(new FileInputStream("button.wav"));
-			    } catch (Exception e) {}
-			    AudioPlayer.player.start(as);
-			    delay();
-			}
-			break;
-		    case 's':
-			if (choice > 0) {
-			    choice--;
-			    try {
-				as = new AudioStream(new FileInputStream("button.wav"));
-			    } catch (Exception e) {}
-			    AudioPlayer.player.start(as);
-			    delay();
-			}
-			break;
-		    case '\n':
-			hasChosen = true;
-			break;
-		    default:
-			break;
-		    }
-		}
-	    } catch (Exception e) {}
-	}
-	c.setColor(new Color(0,255,255));
-	c.fillRect(0,0,300,200);
+        while (!hasChosen) {
+            if (c.isCharAvail()) {
+                switch (c.getChar()) {
+                case 'j':
+                case 's':
+                    if (choice < 3) {
+                        choice++;
+                        try {
+                            as = new AudioStream(new FileInputStream("button.wav"));
+                        } catch (Exception e) {}
+                        AudioPlayer.player.start(as);
+                    }
+                    break;
+                case 'k':
+                case 'w':
+                    if (choice > 0) {
+                        choice--;
+                        try {
+                            as = new AudioStream(new FileInputStream("button.wav"));
+                        } catch (Exception e) {}
+                        AudioPlayer.player.start(as);
+                    }
+                    break;
+                case '\n':
+                    hasChosen = true;
+                    break;
+                default:
+                    break;
+                }
+            }
+            
+            try {
+                BufferedImage menu_bg = ImageIO.read(new File("menu.png"));
+                c.drawImage(menu_bg, 0, 0, null);
+            } catch (FileNotFoundException fnf) {
+                new Message("File Not Found", "Error");
+            } catch (IOException ioe) {
+                new Message("Error reading file", "Error");
+            } catch (NullPointerException npe) {
+                new Message("Thank you for playing this game!", "Thank you!");
+            }
+            drawOutline(choice);
+        }
+        if(choice == 3) {
+            c.close();
+        }
     }
-    public static void main(String[] args) {
-	Console c = new Console();
-    
+    public void run() {
+        menuSelect();
     }
 }
