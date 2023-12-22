0f0c5c23e0635e5542d770f53a041d90d5cd7fef
jorbinjubin <142069933+jorbinjubin@users.noreply.github.com>
2023-12-22 14:58:11 -0500

    Update Main.java with control flow

--- Main.java
+++ Main.java
@@ -4,26 +4,44 @@ import hsa.Message;
 import java.util.*;
 
 public class Main {
-	Console c;
-	public Main() {
-		Console c = new Console("Bicycle Safety");
-	}
-	
-	public static void main (String[] args){
-    	
-		Main m = new Main();
-    	r.splashScreen();
-    	while (true) {
-		r.mainMenu();
-		if (r.menuChoice == 1) break;
-		else if (r.menuChoice == 2) r.instruction ();
-    	//     else if (r.menuChoice == 3) r.viewLeaderboard ();
-    	//     else {
-    	//         if(!r.skipLearning) r.learning();
-    	//         if(!r.skipGameLoop) r.maze();
-    	//         r.gameLoop();
-    	//     }
-    	// }
-    	// r.goodbye ();
-	}	
+    Console c;
+    int menuChoice = 0;
+    public Menu m;
+    public Main() {
+        c = new Console(38,128,"Bicycle Safety"); //1024 by 760
+    }
+    
+    public void menu() {
+        m = new Menu(c);
+        m.run();
+    }
+    public void instr() {
+        c.clear();
+        c.println("entered instructions");
+        c.getChar();
+    }
+    public void play() {
+        c.clear();
+        c.println("entered play");
+        c.getChar();
+    }
+    public void lb() {
+        c.clear();
+        c.println("entered leaderboard");
+        c.getChar();
+    }
+    
+    public static void main(String[] args) {
+        Main z = new Main();
+        while(true) {
+            z.menu();
+            if(Menu.hasChosen) {
+                if(Menu.choice == 0) z.instr();
+                else if(Menu.choice == 1) z.play();
+                else if(Menu.choice == 2) z.lb();
+                //exit is covered in menu();
+            }
+            Menu.hasChosen = false;
+        }
+    }        
 }
