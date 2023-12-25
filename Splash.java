c05323255a895781fbad21e4b460454c291cb77f
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-12-25 11:15:51 -0500

    More like justin what the frick it used to work your fei what the frick update made it not work :roll:

--- Splash.java
+++ Splash.java
@@ -7,41 +7,41 @@ import javax.imageio.*;
 public class Splash{
     Console c;     
     public Splash (Console con) {
-	c = con;
+        c = con;
     }
     
     public void drawCycles() {
-	try { 
-	    Image stripes = ImageIO.read (new File("stripes.jpg"));
-	    Image title = ImageIO.read (new File("title.jpg"));
-	    Image cycles = ImageIO.read(new File("cycles.jpg")); 
-	    Image newScreen = ImageIO.read(new File("continue.jpg"));
-	    stripes = stripes.getScaledInstance(1024, 768, stripes.SCALE_DEFAULT);
-	    title = title.getScaledInstance(605, 354, title.SCALE_DEFAULT);
-	    cycles = cycles.getScaledInstance(1020, 200, stripes.SCALE_DEFAULT);
-	    newScreen = newScreen.getScaledInstance(1024, 768, newScreen.SCALE_DEFAULT);
-	    
-	    c.drawImage(stripes, 0, 0, null);
-	    c.drawImage(title, 210, 40, null);
-	    
-	    for(int i = 0; i < 160; i++) {
-		c.drawImage(cycles, 5, 600-5*i, null);
-		c.drawImage(title, 210, 40, null);
-		try{Thread.sleep(50); } catch(Exception e) {} 
-	    }
-	    
-	    c.drawImage(newScreen, 0, 0, null);
-	}
-	catch (FileNotFoundException fnf) {
-	    new Message("File Not Found","error");
-	}
-	catch(IOException ioe) {
-	    new Message("Error reading file", "error");
-	}
-	
-	c.getChar();
+        try { 
+            Image stripes = ImageIO.read (new File("stripes.jpg"));
+            Image title = ImageIO.read (new File("title.jpg"));
+            Image cycles = ImageIO.read(new File("cycles.jpg")); 
+            Image newScreen = ImageIO.read(new File("continue.jpg"));
+            stripes = stripes.getScaledInstance(1024, 768, stripes.SCALE_DEFAULT);
+            title = title.getScaledInstance(605, 354, title.SCALE_DEFAULT);
+            cycles = cycles.getScaledInstance(1020, 200, cycles.SCALE_DEFAULT);
+            newScreen = newScreen.getScaledInstance(1024, 768, newScreen.SCALE_DEFAULT);
+            
+            c.drawImage(stripes, 0, 0, null);
+            c.drawImage(title, 210, 40, null);
+            
+            for(int i = 0; i < 160; i++) {
+                c.drawImage(cycles, 1, 600-5*i, null);
+                c.drawImage(title, 210, 40, null);
+                try{Thread.sleep(50); } catch(Exception e) {} 
+            }
+            
+            c.drawImage(newScreen, 0, 0, null);
+        }
+        catch (FileNotFoundException fnf) {
+            new Message("File Not Found","error");
+        }
+        catch(IOException ioe) {
+            new Message("Error reading file", "error");
+        }
+        
+        c.getChar();
     }
     public void run() {
-	drawCycles();
+        drawCycles();
     }
 }
