08293ed36416776e9fe23bc0c03cd47a1a8595df
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-12-25 12:46:17 -0500

    added insructions stuff

--- Main.java
+++ Main.java
@@ -13,55 +13,54 @@ public class Main {
     
     //constructor 
     public Main(Menu m, Console con) {
-	//Sets object variables using the 'this' keyword, thanks to Phil Huang for the suggestion
-	this.m = m;
-	this.c = con;
+        //Sets object variables using the 'this' keyword, thanks to Phil Huang for the suggestion
+        this.m = m;
+        this.c = con;
     }
     
     public void menu() {
-	m.run();
+        m.run();
     }
     
     public void splash() {
-	Splash s = new Splash(c);
-	s.run();
+        Splash s = new Splash(c);
+        s.run();
     }
     public void instr() {
-	c.clear();
-	c.println("entered instructions");
-	c.getChar();
+        Instructions i = new Instructions(c); 
+        i.run();
     }
-    
+
     public void play() {
-	c.clear();
-	c.println("entered play");
-	c.getChar();
+        c.clear();
+        c.println("entered play");
+        c.getChar();
     }
     
     public void lb() {
-	c.clear();
-	c.println("entered leaderboard");
-	c.getChar();
+        c.clear();
+        c.println("entered leaderboard");
+        c.getChar();
     }
     
     public static void main(String[] args) {
-	Console c = new Console(38,128,"Bicycle Safety"); //1024 by 760;
-	Menu m = new Menu(c);
-	Main z = new Main(m, c);
-	z.splash();
-	while(true) {
-	    z.menu();
-	    if(m.getHasChosen()) {
-		if(m.getChoice() == 0) z.instr();
-		else if(m.getChoice() == 1) z.play();
-		else if(m.getChoice() == 2) z.lb();
-		else if (m.getChoice() == 3) break;
-	    }
-	    m.setHasChosen(false);
-	}
-	c.clear();
-	c.println("goodbye");
-	c.getChar();
-	z.c.close();
+        Console c = new Console(38,128,"Bicycle Safety"); //1024 by 760;
+        Menu m = new Menu(c);
+        Main z = new Main(m, c);
+        z.splash();
+        while(true) {
+            z.menu();
+            if(m.getHasChosen()) {
+                if(m.getChoice() == 0) z.instr();
+                 else if(m.getChoice() == 1) z.play();
+                 else if(m.getChoice() == 2) z.lb();
+                 else if (m.getChoice() == 3) break;
+            }
+            m.setHasChosen(false);
+        }
+        c.clear();
+        c.println("goodbye");
+        c.getChar();
+        z.c.close();
     }        
 }
