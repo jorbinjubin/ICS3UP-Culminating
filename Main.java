2080ee41204bb84b6420a40358ac167d58be9fab
jorbinjubin <142069933+jorbinjubin@users.noreply.github.com>
2024-01-08 18:49:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,68 +13,69 @@ public class Main {
     
     //constructor 
     public Main(Menu m, Console con) {
-        //Sets object variables using the 'this' keyword, thanks to Phil Huang for the suggestion
-        this.m = m;
-        this.c = con;
+	//Sets object variables using the 'this' keyword, thanks to Phil Huang for the suggestion
+	//For some reason, this is the only way it works
+	this.m = m;
+	this.c = con;
     }
     
     public void menu(int x) {
-        if(x == 1) m.menuSelect();
-        else if(x==2) m.menu2Select();
+	if(x == 1) m.menuSelect();
+	else if(x==2) m.menu2Select();
     }
     
     public void splash() {
-        Splash s = new Splash(c);
-        s.run();
+	Splash s = new Splash(c);
+	s.run();
     }
     public void instr() {
-        Instructions i = new Instructions(c); 
-        i.run();
+	Instructions i = new Instructions(c); 
+	i.run();
     }
     public void maze() {
-        Maze m = new Maze(c); 
-        m.run();
+	Maze m = new Maze(c); 
+	m.run();
     }
 
     public void play() {
-        c.clear();
-        c.println("entered play");
-        c.getChar();
+	c.clear();
+	c.println("entered play");
+	c.getChar();
     }
     
     public void lb() {
-        c.clear();
-        c.println("entered leaderboard");
-        c.getChar();
+	c.clear();
+	c.println("entered leaderboard");
+	c.getChar();
     }
     
     public static void main(String[] args) {
-        Console c = new Console(38,128,"Bicycle Safety"); //1024 by 760;
-        Menu m = new Menu(c);
-        Main z = new Main(m, c);
-        z.splash();
-        while(true) {
-            z.menu(1);
-            if(m.getHasChosen()) {
-                if(m.getChoice() == 0) z.instr();
-                 else if(m.getChoice() == 1) { 
-                    z.menu(2);
-                    if(m.getHasChosen()) {
-                        if(m.getChoice() == 0) z.maze();  
-                        else if(m.getChoice() == 1) z.play(); 
-                        else if(m.getChoice() == 2) continue; 
-                        else if(m.getChoice() == 3) break;
-                    }
-                 }
-                 else if(m.getChoice() == 2) z.lb();
-                 else if (m.getChoice() == 3) break;
-            }
-            m.setHasChosen(false);
-        }
-        c.clear();
-        c.println("goodbye");
-        c.getChar();
-        z.c.close();
-        
+	Console c = new Console(38,128,"Bicycle Safety"); //1024 by 760;
+	Menu m = new Menu(c);
+	Main z = new Main(m, c);
+	z.splash();
+	while(true) {
+	    z.menu(1);
+	    if(m.getHasChosen()) {
+		if(m.getChoice() == 0) z.instr();
+		 else if(m.getChoice() == 1) { 
+		    z.menu(2);
+		    if(m.getHasChosen()) {
+			if(m.getChoice() == 0) z.maze();  
+			else if(m.getChoice() == 1) z.play(); 
+			else if(m.getChoice() == 2) continue; 
+			else if(m.getChoice() == 3) break;
+		    }
+		 }
+		 else if(m.getChoice() == 2) z.lb();
+		 else if (m.getChoice() == 3) break;
+	    }
+	    m.setHasChosen(false);
+	}
+	c.clear();
+	c.println("goodbye");
+	c.getChar();
+	z.c.close();
+	
     }        
 }
