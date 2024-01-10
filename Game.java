68d19c332b122d5f41858d75e5e3b4027f4fbc4a
jorbinjubin <142069933+jorbinjubin@users.noreply.github.com>
2024-01-10 15:13:18 -0500

    Added lights nd stuff

--- Game.java
+++ Game.java
@@ -7,80 +7,68 @@ import javax.imageio.*;
 
 public class Game {
     private Console c;
-    Console debug = new Console("Debugging Window");
+    Console debug = new Console("Debug Screen");
     public Game() {
     //public Game(Console con) {
 	//c = con;
 	c = new Console(38,128,"a Safety");
     }
-    Color road = new Color(96,94,94);
-    Color curb = new Color(162,162,161);
-    Color lamp_glow = new Color(255,255,10,150);
-    Color green = new Color(30,200,30);
+    Color greb = new Color(30,200,30);
     public void drawRoad() {
-	c.setColor(green);
+	c.setColor(greb);
 	c.fillRect(0,0,1024,760);
 	c.setColor(Color.black);
-	c.fillRect(0, 200, 1024, 400);
+	c.fillRect(0, 150, 1024, 470);
     }
-    
     public void barrel(int x) {
 	c.setColor(Color.red);
-	c.fillOval(x, 140, 30, 30);   
-    }
-    
-    public void drawLight(int x) {
-    
-    }
-    
-    public void drawHydrant(int x) {
-    
-    }
-    
-    public void drawSign(int num) {
-    
+	c.fillOval(x, 100, 30, 30);   
+	
+	c.fillOval(x, 500, 30, 30);   
     }
     public void drawBike() {
 	try{
-	    Image cycleRight = ImageIO.read(new File("Bike_ride.png"));
+	    Image cycleRight = ImageIO.read(new File("cycleRight.jpg"));
 	    cycleRight = cycleRight.getScaledInstance(100, 50, cycleRight.SCALE_DEFAULT);
-	    c.drawImage(cycleRight, 500, 520, null);
+	    c.drawImage(cycleRight, 500, 300, null);
 	}catch(IOException ioe) {}
-    }      
+    }
+      
     
     public void anim() {
 	int barrelX = 1024;
 	char ch = ' ';
 	double delta = 0;
 	while(true) {
-	    if(c.isCharAvail()) ch = c.getChar();
+	    if(c.isCharAvail()) {
+	    /*isCharAvail from https://www.staugustinechs.ca/cadawas/hsa/console.html*/
+	   
+		ch = c.getChar();
+	    } 
 	    else {
-		if(Math.abs(0-delta) < 0.3) delta = 0; //this just checks if the speed is close enough to zero and stops it.
-		if(delta != 0) { //Slowly slows down because gameplay.
-		    if(delta > 0) delta -= delta * 0.2 ;
-		    if(delta < 0) delta += delta * -0.3 ;
-		}
+		ch = ' ';
+		if(delta > 0.5) delta -= 0.3*delta;
+		else if(delta < -0.5) delta += 0.3*delta;
+		else if(delta < 0.5 && delta > -0.5) delta = 0;
 	    }
+	    debug.println(delta);
+	    debug.println(barrelX);
 	    if(ch == 'o') break;
 	    if(ch == 'd') delta+=3;
 	    if(ch == 'a') delta-=3;
 	    if(delta > 10) delta = 10;
-	    if(delta < -5) delta = -5;
+	    if(delta < -10) delta = -10;
 	    barrelX -= delta;
 	    drawRoad();
 	    drawBike();
-	    debug.println(delta);
 	    barrel((int)barrelX);
-	    barrel((int)barrelX + 300);
-	    barrel((int)barrelX + 600);
-	    barrel((int)barrelX + 900);
-	    ch = ' ';
-	    try{Thread.sleep(48);} catch(Exception e) {}
-	    if(c.isCharAvail()) ch = c.getChar();
-	    
+	    if(barrelX < 000) {
+		break;
+	    }
+	    try{Thread.sleep(100);} catch(Exception e) {}
 	}
 	c.clear();
-	c.println("dieded");
+	debug.println("dieded");
     }
     
     public static void main(String[] args) {
