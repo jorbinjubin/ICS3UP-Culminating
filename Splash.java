c46e5afd18c7c48395ab179d00b185202df7866f
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2024-01-14 22:14:32 -0500

    Update Splash.java

--- Splash.java
+++ Splash.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
-public class Splash{
+public class Splash {
     /*
     * variable declaration
     * ------------------------------------------------------------------------------------
@@ -22,49 +22,60 @@ public class Splash{
     
     // constructor 
     public Splash (Console con) {
-	c = con;
+        c = con;
     }
     
     // draws the splash screen
+    /*
+    * variable declaration
+    * ------------------------------------------------------------------------------------
+    * Type          | Name          | Description
+    * Image         | stripes       | image of the background stripes
+    * Image         | title         | image of the title
+    * Image         | cycles        | image of the bicycles
+    * Image         | newScreen     | image of the contnue screen
+    * ------------------------------------------------------------------------------------
+    */
     public void drawCycles() {
-	try { 
-	    // gets the images
-	    Image stripes = ImageIO.read (new File("stripes.jpg"));
-	    Image title = ImageIO.read (new File("title.jpg"));
-	    Image cycles = ImageIO.read(new File("cycles.jpg")); 
-	    Image newScreen = ImageIO.read(new File("continue.jpg"));
-	    stripes = stripes.getScaledInstance(1024, 768, stripes.SCALE_DEFAULT);
-	    title = title.getScaledInstance(605, 354, title.SCALE_DEFAULT);
-	    cycles = cycles.getScaledInstance(1020, 200, cycles.SCALE_DEFAULT);
-	    newScreen = newScreen.getScaledInstance(1024, 768, newScreen.SCALE_DEFAULT);
-	    
-	    // draws the stripes and the title
-	    c.drawImage(stripes, 0, 0, null);
-	    c.drawImage(title, 210, 40, null);
-	    
-	    // animated part
-	    for(int i = 0; i < 160; i++) {
-		c.drawImage(cycles, 1, 600-5*i, null);
-		c.drawImage(title, 210, 40, null);
-		try{Thread.sleep(50); } catch(Exception e) {} 
-	    }
-	    
-	    // draws the next screen
-	    c.drawImage(newScreen, 0, 0, null);
-	}
-	catch (FileNotFoundException fnf) {
-	    new Message("File Not Found","error");
-	}
-	catch(IOException ioe) {
-	    new Message("Error reading file", "error");
-	}
-	
-	// pauses the program until the user presses a key
-	c.getChar();
+        try { 
+            // gets the images
+            Image stripes = ImageIO.read (new File("stripes.jpg"));
+            Image title = ImageIO.read (new File("title.jpg"));
+            Image cycles = ImageIO.read(new File("cycles.jpg")); 
+            Image newScreen = ImageIO.read(new File("continue.jpg"));
+		
+	    // code for changing size of image from https://stackoverflow.com/questions/5895829/resizing-image-in-java
+            stripes = stripes.getScaledInstance(1024, 768, stripes.SCALE_DEFAULT);
+            title = title.getScaledInstance(588, 312, title.SCALE_DEFAULT);
+            cycles = cycles.getScaledInstance(1020, 200, cycles.SCALE_DEFAULT);
+            
+            // draws the stripes and the title
+            c.drawImage(stripes, 0, 0, null);
+            c.drawImage(title, 210, 40, null);
+            
+            // animated part
+            for(int i = 0; i < 80; i++) {
+                c.drawImage(cycles, 1, 600-10*i, null);
+                c.drawImage(title, 210, 40, null);
+                try{Thread.sleep(120); } catch(Exception e) {} 
+            }
+            
+            // draws the next screen
+            c.drawImage(newScreen, 0, 0, null);
+        }
+        catch (FileNotFoundException fnf) {
+            new Message("File Not Found","error");
+        }
+        catch(IOException ioe) {
+            new Message("Error reading file", "error");
+        }
+        
+        // pauses the program until the user presses a key
+        c.getChar();
     }
     
     // runs drawCycles
     public void run() {
-	drawCycles();
+        drawCycles();
     }
 }
